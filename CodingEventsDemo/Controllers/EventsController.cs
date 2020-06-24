46368469ab496f350d515c445986b0508ae28035
mtorres9095 <mtorres9095@gmail.com>
2020-06-24 13:42:35 -0400

    Completed the Add

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -2,32 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEventsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace coding_events_practice.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        static private List<Event> Events = new List<Event>();
         // GET: /<controller>/
         public IActionResult Index()
         {
-            //Events.Add("Code With Pride", "Gigs from all over the world meeting to code with Daryn");
-            //Events.Add("Apple WWDC", "Coders from the Reston area meeting to code with Daryn");
-            //Events.Add("Strange Loop", "Coders from Maryland meeting to code with Daryn");
             ViewBag.events = Events;
             return View();
         }
         [HttpGet]
-        public IActionResult AddEvent()
+        public IActionResult Add()
         {
             return View();
         }
+
         [HttpPost]
-        [Route("Events/AddEvent")]
+        [Route("Events/Add")]
         public IActionResult NewEvent(string name, string desc)
         {
-            Events.Add(name, desc);
+            Events.Add(new Event(name, desc));
+
             return Redirect("/Events");
 
         }
---
+++ CodingEventsDemo/Models/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Models
+{
+    public class Event
+    {
+        public string Name {get; set;}
+        public string Description { get; set;}
+        //public int Id { get; }
+        //private static int nextId = 1;
+
+        public Event (string name, string description)
+        {
+            Name = name;
+            Description = description;
+            //Id = nextId;
+            //nextId++;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
