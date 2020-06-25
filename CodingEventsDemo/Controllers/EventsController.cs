be39643eeab2f772e66b7dd328a893f974592f9a
mtorres9095 <mtorres9095@gmail.com>
2020-06-24 20:29:47 -0400

    Completed Code with Edit Functions

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +10,10 @@ namespace coding_events_practice.Controllers
 {
     public class EventsController : Controller
     {
-        static private List<Event> Events = new List<Event>();
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
             return View();
         }
         [HttpGet]
@@ -24,12 +24,45 @@ namespace coding_events_practice.Controllers
 
         [HttpPost]
         [Route("Events/Add")]
-        public IActionResult NewEvent(string name, string desc)
+        public IActionResult NewEvent(string name, string description)
         {
-            Events.Add(new Event(name, desc));
+            EventData.Add(new Event(name, description));
 
             return Redirect("/Events");
+        }
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach(int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+            return Redirect("/Events");
 
+        }
+        //GET: /events/edit/{id}
+        [HttpGet("/events/edit/{id}")]
+        [Route("events/edit/{id}")]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.eventToEdit = EventData.GetById(id);
+            return View();
+        }
+        //Post:/events/edit
+        [HttpPost]
+        [Route("/events/edit")]
+        public IActionResult Update(int id, string name, string description)
+        {
+            Event eventToUpdate = EventData.GetById(id);
+            eventToUpdate.Name = name;
+            eventToUpdate.Description = description;
+
+            return Redirect("/events");
         }
     }
 
--- CodingEventsDemo/Models/Event.cs
+++ CodingEventsDemo/Models/Event.cs
@@ -1,28 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEventsDemo.Models
 {
     public class Event
     {
-        public string Name {get; set;}
-        public string Description { get; set;}
-        //public int Id { get; }
-        //private static int nextId = 1;
+        public string Name { get; set; }
+        public string Description { get; set; }
 
-        public Event (string name, string description)
+        public int Id { get; }
+        static private int nextId = 1;
+
+        public Event()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Event(string name, string description) : this()
         {
             Name = name;
             Description = description;
-            //Id = nextId;
-            //nextId++;
         }
+
         public override string ToString()
         {
             return Name;
         }
 
+        public override bool Equals(object obj)
+        {
+            return obj is Event @event &&
+                   Id == @event.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
     }
 }
