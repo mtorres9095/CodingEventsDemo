723707fc08051c5c20a4503be1945d5dbc989035
mtorres9095 <mtorres9095@gmail.com>
2020-07-09 11:40:34 -0400

    Chapter 19 Studio

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEventsDemo.Controllers
@@ -22,6 +22,45 @@ namespace CodingEventsDemo.Controllers
 
             return View(categories);
         }
+
+        //GET: EventCategory/Create
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel viewModel = new AddEventCategoryViewModel();
+            return View(viewModel);
+
+            //AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            //return View(addEventCategoryViewModel);
+            
+        }
+
+        //Post: EventCategory/Create
+        [HttpPost]
+        public IActionResult Create(AddEventCategoryViewModel viewModel)
+        // public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+         {
+                if (ModelState.IsValid)
+                {
+                    EventCategory eventCategory = new EventCategory
+                    //EventCategory newEventCategory = new EventCategory // eventCategory for new...
+                    {
+                        
+                        Name = viewModel.Name
+                        //Name = addEventCategoryViewModel.Name //viewModel only
+
+                    };
+
+                    context.Categories.Add(eventCategory); // .Category
+                    context.SaveChanges();//new 
+                     // context.Add(newEventCategory); // .Category  delete new and change tp event..
+                    
+                    return Redirect("/EventCategory");
+
+                }
+             return View("Create", viewModel);
+            // return View(addEventCategoryViewModel);
+         }
+
         
     }
 }
---
+++ CodingEventsDemo/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CodingEventsDemo.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required(ErrorMessage = "Category Name is required.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Category Name must be between 3 and 20 characters.")]
+        public string Name { get; set; }
+
+        
+    }
+}
